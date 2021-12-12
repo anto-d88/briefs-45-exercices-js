eb29ec9f2323b71f67ad968345ff8589e0c7805a
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 20:49:01 +0100

    exercices 37 complete

--- 12-while-for/5-replace-for-while/script.js
+++ 12-while-for/5-replace-for-while/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let i = 0;
+
+while(i < 3){
+
+      alert(`number ${i}`);
+      i++;
+}
