5ccea1c475ad7d54f46ee9c4961eeef14a02391c
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 20:42:01 +0100

    exercices 35 complete

--- 12-while-for/3-which-value-for/script.js
+++ 12-while-for/3-which-value-for/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/* 
+LES DEUX BOUCLE AFFICHE 4 EN DERNIER 
+
 */
