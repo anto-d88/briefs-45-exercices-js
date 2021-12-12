d6fb75de4aeb7fd9f28cd25ca03c893c36fb7498
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 16:27:37 +0100

    exercices 7 complete

--- 04-operators/2-assignment-result/script.js
+++ 04-operators/2-assignment-result/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+/*  a = 4
+    x = 5
+    
+    a est multipler par 2 et x equiveau à 1 plus a multiplier par 2*/
