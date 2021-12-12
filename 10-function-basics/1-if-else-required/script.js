f4e5659b4947945dde537249e29ddcdaf74a544b
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 19:28:10 +0100

    exercices 28 complete

--- 10-function-basics/1-if-else-required/script.js
+++ 10-function-basics/1-if-else-required/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/* 
+
+oui ça fonctionne toujour 
 */
