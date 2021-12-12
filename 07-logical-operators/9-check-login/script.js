9ac2121813e8919aef624bfee1eac9228ac9b27c
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 18:21:59 +0100

    exercices 24 complete

--- 07-logical-operators/9-check-login/script.js
+++ 07-logical-operators/9-check-login/script.js
@@ -1,4 +1,20 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let userName = prompt("Who' you (oohooh! oohooh!");
+
+if(userName == "Admin"){
+      let password = prompt("Password?");
+
+      if( password == "TheMaster"){
+            alert("Welcom!");
+      }
+
+      else if( password == null || password == ""){
+            alert("Canceled");
+      }
+      else {
+            alert("Wrong password");
+      }
+}
+
+else{
+      alert("Canceled");
+}
