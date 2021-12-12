5b4e51caeee3c920216603d3570f6f4271d99a9a
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 19:43:35 +0100

    exercices 29 complete

--- 10-function-basics/2-rewrite-function-question-or/script.js
+++ 10-function-basics/2-rewrite-function-question-or/script.js
@@ -1,4 +1,13 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function checkAge(age){
+
+return (age > 18) || "Did parents allow you?";
+}
+
+alert(checkAge(prompt("How old are you?")));
+
+function checkAge2(age){
+
+return (age > 18) ? true : "Did parents allow you?";
+}
+
+alert(checkAge2(prompt("How old are you?")));
