c3bc616a111296040cbc5edf158316024fab1612
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 20:10:50 +0100

    exercices 31 complete

--- 10-function-basics/4-pow/script.js
+++ 10-function-basics/4-pow/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function pow(x, n){
+      if(n > 0) {
+            return (x ** parseInt(n));
+      }
+
+      else{
+            "Le nombre doit être superieur à 'n'";
+      }
+}
+
+alert(pow(+prompt("Entrez un  nombre"), +prompt("Entrez un second nombre")));
