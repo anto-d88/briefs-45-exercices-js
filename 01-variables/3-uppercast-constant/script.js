cf58102cdc660f05615aa4825c22a370d5008107
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 14:20:47 +0100

    exercices 3 complete

--- 01-variables/3-uppercast-constant/script.js
+++ 01-variables/3-uppercast-constant/script.js
@@ -1,4 +1,6 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+/*  il est juste d'utiliser BIRTHDAY en majuscule car c'est une constante coder en "DUR"
+qui ne changera pas pendant l'execution.
+age ne doit pas etre en majuscule car c'est une constante qui peut changer pendant l'execution.
 */
