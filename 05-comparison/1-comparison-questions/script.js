e7dd35d0c9faf3b3a879320ba9ea2825447c476b
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 16:48:33 +0100

    exercices 10 complete

--- 05-comparison/1-comparison-questions/script.js
+++ 05-comparison/1-comparison-questions/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/*  
+true
+false
+true
+false
+false
+false
 */
