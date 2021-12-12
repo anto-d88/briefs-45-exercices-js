6f9cabd2e0171a87099c9eb449fdac83f61f9d11
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 16:43:24 +0100

    exercices 9 complete

--- 04-operators/4-fix-prompt/script.js
+++ 04-operators/4-fix-prompt/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/*  les valeur renvoyer dans prompt sont automatiquement 
+des string donc avec une adition les deux valeur sont concaténer.
+il faudra donc les convertir avec + ou Number()
 */
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
+
+alert(a+b);
