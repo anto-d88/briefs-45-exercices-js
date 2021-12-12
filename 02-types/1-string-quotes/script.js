eafe968b0ade7a1b12ac3a857314dc187d45702d
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 14:28:40 +0100

    exercices 4 complete

--- 02-types/1-string-quotes/script.js
+++ 02-types/1-string-quotes/script.js
@@ -1,4 +1,10 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+let name = "Ilya";
+
+alert( `hello ${1}`);
+alert(`hello ${"name"}`);
+alert(`hello ${name}`);
+/* 
+hello 1
+hello name
+hello Ilia
 */
