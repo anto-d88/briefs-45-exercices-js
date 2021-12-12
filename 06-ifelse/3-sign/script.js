d0b225c884506bcf281506267153204102ef10aa
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 17:04:03 +0100

    exercices 13 complete

--- 06-ifelse/3-sign/script.js
+++ 06-ifelse/3-sign/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let nombre = prompt("Ecrivez un npmbre");
+
+if(nombre > 0){
+      alert("1");
+}
+else if(nombre < 0){
+      alert("-1");
+}
+else{
+      alert("0");
+};
