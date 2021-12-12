2804bba03bd7986f0434df82159c0f293417d00b
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 20:46:15 +0100

    exercices 36 complete

--- 12-while-for/4-for-even/script.js
+++ 12-while-for/4-for-even/script.js
@@ -2,3 +2,4 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+for(i = 2; i <= 10; i+=2) alert(i);
