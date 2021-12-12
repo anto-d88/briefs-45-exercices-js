d7bc86a429f9ad843208a796cadb5f12fde58047
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 16:53:32 +0100

    exercices 11 complete

--- 06-ifelse/1-if-zero-string/script.js
+++ 06-ifelse/1-if-zero-string/script.js
@@ -1,4 +1,3 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+
+/*  oui alert s'affiche */
