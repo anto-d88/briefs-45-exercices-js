b70340a235dd095d90368cdc00454994aa2d773a
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 16:59:48 +0100

    exercices 12 complete

--- 06-ifelse/2-check-standard/script.js
+++ 06-ifelse/2-check-standard/script.js
@@ -1,3 +1,11 @@
+let check= prompt('quel est le nom officiel de Javascript?');
+
+if(check == "ECMAScript"){
+      alert("Bonne réponse!");
+}
+else{
+      alert("Ne sait pas? ECMAScript!");
+};
 /*  Votre code Javascript vient ici:
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
