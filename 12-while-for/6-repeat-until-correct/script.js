eb63b43f2a0d8970bfa9fb60bfa59b98b1d8257f
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 20:52:32 +0100

    exercices 38 complete

--- 12-while-for/6-repeat-until-correct/script.js
+++ 12-while-for/6-repeat-until-correct/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let i = 0;
+
+while( i <= 100){
+
+      i = +prompt("Entrez un nombre superieur à 100");
+}
+
+alert("welcom!");
