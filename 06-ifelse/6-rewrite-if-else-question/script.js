85b0113945d9d93187fba0758b99a7962fff57a1
anto-88 <anto-88@pop-os.localdomain>
2021-12-12 17:22:04 +0100

    exercices 15 complete

--- 06-ifelse/6-rewrite-if-else-question/script.js
+++ 06-ifelse/6-rewrite-if-else-question/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let message = (login == "Employee") ? "Hello" :
+              
+              (login == "Director") ? "Greeting" :
+
+              (login == "") ? "No login" : "";
